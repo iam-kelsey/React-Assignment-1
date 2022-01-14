40dee633eff4f6dec89e0da4bc1e199b797d0af9
Kelsey <71285442+iam-kelsey@users.noreply.github.com>
2022-01-13 21:01:25 -0800

    Modified

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 
 
@@ -14,9 +15,42 @@ function MyApp() {
     setCharacters(updated);
  }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 201)
+      setCharacters([...characters, person] );
+    });
   }
+
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:5000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+  }
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+  }, [] );
 
 
   return (
